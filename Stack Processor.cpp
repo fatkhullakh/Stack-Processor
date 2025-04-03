26f6e5773f9529e7b459e951b058d57f82a84520
fatkhullakh <tfatkh@gmail.com>
2025-04-03 10:29:12 +0200

    Just checking

--- Stack Processor.cpp
+++ Stack Processor.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <vector>
 using namespace std;
 
 
 class StackProcessor {
-
 private:
 
 
 public:
-
-
 };
 
 
 
 int main() {
-
+	string programStr, inputStr;
+	getline(cin, programStr);
+	getline(cin, inputStr);
 
 	return 0;
 }
